ad962426d2a84d0109de9c10d8c2085a60fc5b1d
rgmelko <rgmelko@Dolomite.local>
2019-12-16 22:20:59 -0500

    Added XOR spin flip

--- updates.jl
+++ updates.jl
@@ -13,9 +13,10 @@ function LinkedList()
     Associates = zeros(Int,0)  
 
 	First = zeros(Int,0)  #scope is this function only
+    #The first N elements of the linked list are the spins of the LHS basis state
 	for i = 1:nSpin
 		push!(First,i)
-		push!(LinkList,-99)
+		push!(LinkList,-99) #we don't yet know what these link to
 		push!(LegType,spin_left[i])
 		#push!(Associates,spin_left[i])
 	end #i
@@ -26,10 +27,13 @@ function LinkedList()
 
     spin_prop = copy(spin_left)  #the propagated spin state
 
+    #Now, add the 2M operators to the linked list.  Each has either 2 or 4 legs
     for i = 1:2*M  #size of the operator list
 
-       if operator_list[i,1] == -2
-           println("off-diagonal bond operator at ",i)
+       if operator_list[i,1] == -2  #off-diagonal site operator
+		   site = operator_list[i,2]
+           spin_prop[i] = xor(spin_prop[i],1) #spinflip
+
        elseif operator_list[i,1] == -1
            println("diagonal site operator at ",i)
 	   else
