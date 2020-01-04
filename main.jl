72d3ccc510dad737d376e64aac2e7edd2beeae07
Roger Melko <rgmelko@uwaterloo.ca>
2020-01-04 14:06:19 -0500

    Removed operator_list initialization

--- main.jl
+++ main.jl
@@ -8,7 +8,7 @@ include("measurements.jl") #functions for the Monte Carlo updates
 
 #Projector parameters
 M = 300 #length of the projector operator_list is 2M
-h_x = 1.0
+h_x = 0.5
 J_ = 1.0
 MCS = 10000 #the number of Monte Carlo steps
 
@@ -24,21 +24,6 @@ operator_list = fill(0,(2*M,2))
 #  (0,0) is the identity operator I - NOT USED IN THE PROJECTOR CASE
 #  (i,j) is a diagonal bond operator J(sigma^z_i sigma^z_j)
 #*******  Globals
-
-#initialize the operator list
-for i = 1:2*M
-    coin = rand(Bool)
-    if coin == true #insert a diagonal site operator
-        site = rand(1:nSpin)
-        operator_list[i,1] = -1
-        operator_list[i,2] = site
-    else
-        bond = rand(1:nBond)
-        operator_list[i,1] = bond_spin[bond,1]
-        operator_list[i,2] = bond_spin[bond,2]
-    end
-end
-
 
 for i = 1:2000  #Equilibration
     DiagonalUpdate()
